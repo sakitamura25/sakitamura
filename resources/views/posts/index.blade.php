09c334f971aab9c7ecaca5cbca72781723ae81b7
sakitamura25 <t.per125@icloud.com>
2022-12-16 21:17:35 +0900

    formファサード。 送信ボタンの挿入。

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,4 +3,12 @@
 @section('content')
 <h2>機能を実装していきましょう。</h2>
 
+{!! Form::open(['url' => '', 'method' => 'post']) !!}
+
+<img src="mages/dawn.png" alt="">
+
+{{ Form::button('<img src="images/post.png" alt="送信">', ['class' => 'btn', 'type' => 'submit']) }}
+
+{!! Form::close() !!}
+
 @endsection
