fdc0cc349094e6f58aa2f41560bacaf159c1954f
sakitamura25 <t.per125@icloud.com>
2022-12-16 21:53:19 +0900

    commentにplaceholderを追加。

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.login')
 
 @section('content')
-<h2>機能を実装していきましょう。</h2>
+<!-- <h2>機能を実装していきましょう。</h2> -->
 
 {!! Form::open(['url' => '', 'method' => 'post']) !!}
 
-<img src="mages/dawn.png" alt="">
+  <img src="images/dawn.png" alt="">
+
+{{ Form::text('comment', null, ['placeholder' => '何をつぶやこうか…？']) }}
 
 {{ Form::button('<img src="images/post.png" alt="送信">', ['class' => 'btn', 'type' => 'submit']) }}
 
