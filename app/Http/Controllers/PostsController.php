8096b1d2912200f65dd40a0a0e5736bba8bed83d
sakitamura25 <t.per125@icloud.com>
2022-12-22 21:37:42 +0900

    PostsCntrollerの修正。

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Auth;
 
 class PostsController extends Controller
 {
     //
     public function index(){
         return view('posts.index');
+    }
+
+    public function create(Request $request){
+        $post = $request->input('newPost');
+        DB::table('posts')->insert([
+            'user_id' => Auth::user()->id,
+            'posts' => $request->posts
+        ]);
+
+        return back();
     }
 }
