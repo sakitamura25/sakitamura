7b819bd966713ce5cb9c3fd87b2ce418cba499a3
sakitamura25 <t.per125@icloud.com>
2022-12-22 21:36:22 +0900

    投稿フォームの修正。

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,13 +3,11 @@
 @section('content')
 <!-- <h2>機能を実装していきましょう。</h2> -->
 
-{!! Form::open(['url' => '', 'method' => 'post']) !!}
+{!! Form::open(['url' => 'post/create', 'method' => 'post']) !!}
 
-  <img src="images/dawn.png" alt="">
+{!! Form::input('text', 'newPost', null, ['required', 'placeholder' => '何をつぶやこうか…？']) !!}
 
-{{ Form::text('comment', null, ['placeholder' => '何をつぶやこうか…？']) }}
-
-{{ Form::button('<img src="images/post.png" alt="送信">', ['class' => 'btn', 'type' => 'submit']) }}
+{!! Form::button('<img src="images/post.png" alt="送信">', ['class' => 'btn', 'type' => 'submit']) !!}
 
 {!! Form::close() !!}
 
