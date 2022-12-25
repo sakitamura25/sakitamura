76e805d971b134d3103e5dc8415cfdfff3019dc4
sakitamura25 <t.per125@icloud.com>
2022-12-25 12:22:59 +0900

    投稿の表示。

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,20 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\POST;
 use Auth;
+use Illuminate\Support\Facades\Validator;
+
 
 class PostsController extends Controller
 {
-    //
     public function index(){
-        return view('posts.index');
+        $posts = Post::latest()->first();
+        return view('posts.index', ['posts' => $posts]);
     }
 
     public function create(Request $request){
         $post = $request->input('newPost');
         DB::table('posts')->insert([
             'user_id' => Auth::user()->id,
-            'posts' => $request->posts
+            'posts' => $post
         ]);
 
         return back();
