f05289242ae393c3353e7d17215c4160fc7b3db3
sakitamura25 <t.per125@icloud.com>
2023-01-09 13:41:49 +0900

    プロフィール画面の作成。

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -2,6 +2,28 @@
 
 @section('content')
 
+{!! Form::open(['url' => '/profile', 'method' => 'post']) !!}
 
+{!! Form::hidden('id, $user->id') !!}
+
+{!! Form::label('UserName') !!}
+{!! Form::input('text', 'upUserName', $user->username, ['required']) !!}
+
+{!! Form::label('MailAddress') !!}
+{!! Form::input('mail', 'upMail', $user->mail, ['required']) !!}
+
+{!! Form::label('Password') !!}
+{!! Form::input('password', '', $user->password, ['readonly']) !!}
+
+{!! Form::label('new Password') !!}
+{!! Form::password('newPassword') !!}
+
+{!! Form::label('Bio') !!}
+{!! Form::input('text', 'bio', $user->bio) !!}
+
+{!! Form::label('Icon Image') !!}
+
+<button type="submit">更新</button>
+{!! Form::close() !!}
 
 @endsection
