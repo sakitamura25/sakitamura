f5bd0363ca8452fb4aca5a6e505ce987616c98df
sakitamura25 <t.per125@icloud.com>
2023-01-09 13:39:32 +0900

    投稿の編集。

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Validator;
 class PostsController extends Controller
 {
     public function index(){
-        $posts = DB::table('posts')
+        $post = DB::table('posts')
             ->select([
                 'users.username',
                 'posts.user_id',
@@ -24,17 +24,27 @@ class PostsController extends Controller
                 $join->on('posts.user_id', '=', 'users.id');
             })
             ->get();
-        return view('posts.index', ['posts' => $posts]);
+        return view('posts.index', ['posts' => $post]);
     }
 
     public function create(Request $request){
-        $posts = $request->input('newPost');
+        $post = $request->input('newPost');
         DB::table('posts')->insert([
             'user_id' => Auth::user()->id,
-            'posts' => $posts
+            'posts' => $post
         ]);
 
         return back();
+    }
+
+    public function update(Request $request, $id){
+        $id = $request->input('id');
+        $up_post = $request->input('upPost');
+        DB::table('posts')
+            ->where('id', $id)
+            ->update(['posts' => $up_post]);
+
+        return redirect('/top');
     }
 
     public function delete($id){
