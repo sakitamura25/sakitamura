610cf58e64ae16b551b2d20de5dfdc0434007a7e
sakitamura25 <t.per125@icloud.com>
2023-01-19 22:13:34 +0900

    フォロー、アンフォローの追加。

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use DB;
 
 class FollowsController extends Controller
 {
+
     //
     public function followList(){
         return view('follows.followList');
@@ -13,4 +16,29 @@ class FollowsController extends Controller
     public function followerList(){
         return view('follows.followerList');
     }
+
+    public function follow(Request $request){
+        $follower = Auth::user()->id;
+        $follow = $request->input('id');
+
+        DB::table('follows')->insert([
+            'follower' => $follower,
+            'follow' => $follow,
+        ]);
+
+        return redirect('/search');
+    }
+
+    public function unfollow(Request $request){
+        $follower = Auth::user()->id;
+        $follow = $request->input('id');
+
+        DB::table('follows')
+            ->where('follower', $follower)
+            ->where('follow', $follow)
+            ->delete();
+
+        return redirect('/search');
+    }
+
 }
