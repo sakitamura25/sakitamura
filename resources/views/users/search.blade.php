3bb960bf759dc1eec39848de45354dbb5bf8011c
sakitamura25 <t.per125@icloud.com>
2023-01-19 22:12:34 +0900

    ユーザー検索フォームの作成。

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
 
+<div>
+  {{ Form::open(['url' => '/search', 'method' => 'post']) }}
+  {{ Form::input('text', 'keyword', $keyword, ['placeholder' => 'ユーザー名']) }}
+  {{ Form::button('<i class="fas fa-paper-plane" aria-hidden="true" alt="検索"></i>', ['class' => 'btn', 'type' => 'submit']) }}
+  {{ Form::close()}}
+</div>
 
+
+@foreach($users as $user)
+<tr>
+  <li>{{ $user->username }}</li>
+    @if($follows->contains($user->id))
+      {{ Form::open(['url' => '/unfollow', 'method' => 'post']) }}
+      {{ Form::hidden('id', $user->id) }}
+      {{ Form::button('フォローをはずす', ['class' => 'btn', 'type' => 'submit']) }}
+      {{ Form::close() }}
+
+    @else
+      {{ Form::open(['url' => '/follow', 'method' => 'post']) }}
+      {{ Form::hidden('id', $user->id) }}
+      {{ Form::button('フォローする', ['class' => 'btn', 'type' => 'submit']) }}
+      {{ Form::close() }}
+
+    @endif
+</tr>
+@endforeach
 
 @endsection
