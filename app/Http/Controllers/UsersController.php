6be45fda16f6bee8f44917f7c459b81c34e94e2c
sakitamura25 <t.per125@icloud.com>
2023-01-19 22:10:38 +0900

    ユーザー検索の作成。

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use App\User;
+use Illuminate\Support\Facades\DB;
+
 
 
 class UsersController extends Controller
@@ -18,26 +21,53 @@ class UsersController extends Controller
 
     public function profileUpdate(Request $request){
 
-        $request = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'username' => 'required|between:4,12',
-            'mail' => ['required', 'email', 'between:4,12', Rule::unique('users')->ignore(Auth::id())],
-            'new_password' => 'alpha_num|between:4,12|unique:users',
+            // 'mail' => 'required|email|between:4,12|unique:users',
+            // 'password' => 'required|alpha_num|between:4,12|unique:users',
             'bio' => 'max:200',
-        ])->validate();
+        ]);
+
+        if ($validator->fails()) {
+
+            return redirect('/profile');
+                // ->withError($validator)
+                // ->withInput();
+
+        } else {
 
         $user = Auth::user();
         $user->username = $request->input('upUserName');
         $user->mail = $request->input('upMail');
         $user->password = bcrypt($request->input('newPassword'));
         $user->bio = $request->input('bio');
+        // $user->images = $request->file('images')->store('public/images');
         $user->save();
 
+        return view('users.profile');
+        }
 
-
-        return redirect('/top');
     }
 
-    public function search(){
-        return view('users.search');
+    public function index(Request $request){
+        $keyword = $request ->input('keyword');
+        $id = Auth::id();
+        $query = DB::table('users')
+            ->where('id', '!=', $id);
+
+        $follows = DB::table('follows')
+            ->where('follower', Auth::id())
+            ->pluck('follow');
+
+        if (!empty($keyword)) {
+            $query->where('username', 'LIKE', "%{$keyword}%")
+                ->get();
+        }
+            $users = $query->get();
+
+        return view('users.search', compact('keyword', 'users', 'follows'));
     }
+
+
+
 }
