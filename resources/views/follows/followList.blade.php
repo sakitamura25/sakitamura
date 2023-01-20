6ff8602c24049b1591cd8450801bf4568736da8d
sakitamura25 <t.per125@icloud.com>
2023-01-20 22:12:38 +0900

    フォローリスト、フォロワーリストのアイコンの表示。

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -2,4 +2,8 @@
 
 @section('content')
 
+  @foreach($follow_lists as $follow_list)
+    <img src="{{ asset('images/' . $follow_list->images) }}" alt="{{$follow_list->images }}">
+  @endforeach
+
 @endsection
--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.login')
 
 @section('content')
+
+  @foreach($follower_lists as $follower_list)
+    <img src="{{ asset('images/' . $follower_list->images) }}" alt="{{$follower_list->images }}">
+  @endforeach
+
 @endsection
