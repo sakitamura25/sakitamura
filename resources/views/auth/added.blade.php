b9b6d793c67bcb73dc8456b19cbb9ea8d69e8a4e
sakitamura25 <t.per125@icloud.com>
2023-01-25 21:12:09 +0900

    added.blade.phpのCSSを追加。

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -3,14 +3,18 @@
 @section('content')
 
 <div id="clear">
-@if(session('username'))
-<p> {{ session('username') }}さん、</p>
-@endif
-<p>ようこそ！DAWNSNSへ！</p>
-<p>ユーザー登録が完了しました。</p>
-<p>さっそく、ログインをしてみましょう。</p>
+  <div class="added-copy">
+    @if(session('username'))
+      <p class="added-username"> {{ session('username') }}さん</p>
+    @endif
+    <p>ようこそ！DAWNSNSへ！</p>
+  </div>
+  <div class="added-text">
+    <p>ユーザー登録が完了しました。</p>
+    <p>さっそく、ログインをしてみましょう。</p>
+  </div>
 
-<p class="btn"><a href="/login">ログイン画面へ</a></p>
+  <p class="added-btn"><a href="/login", class="to-login-btn">ログイン画面へ</a></p>
 </div>
 
 @endsection
