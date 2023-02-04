8111db72b7731bc4c7379a00ac900360e841433f
sakitamura25 <t.per125@icloud.com>
2023-02-04 23:22:44 +0900

    ユーザー検索画面の修正。

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,32 +1,31 @@
 @extends('layouts.login')
 
 @section('content')
-
-<div>
-  {{ Form::open(['url' => '/search', 'method' => 'post']) }}
-  {{ Form::input('text', 'keyword', $keyword, ['placeholder' => 'ユーザー名']) }}
-  {{ Form::button('<i class="fas fa-paper-plane" aria-hidden="true" alt="検索"></i>', ['class' => 'btn', 'type' => 'submit']) }}
-  {{ Form::close()}}
-</div>
-
+  <div id="search-container">
+    {{ Form::open(['url' => '/search', 'method' => 'post']) }}
+    {{ Form::input('text', 'keyword', $keyword, ['class' => 'search-form', 'placeholder' => 'ユーザー名']) }}
+    {{ Form::button('<i class="fa-solid fa-magnifying-glass" aria-hidden="true" alt="検索"></i>', ['class' => 'search-button btn', 'type' => 'submit']) }}
+    {{ Form::close()}}
+  </div>
 
 @foreach($users as $user)
-<tr>
-  <li>{{ $user->username }}</li>
-    @if($follows->contains($user->id))
-      {{ Form::open(['url' => '/unfollow', 'method' => 'post']) }}
-      {{ Form::hidden('id', $user->id) }}
-      {{ Form::button('フォローをはずす', ['class' => 'btn', 'type' => 'submit']) }}
-      {{ Form::close() }}
-
-    @else
-      {{ Form::open(['url' => '/follow', 'method' => 'post']) }}
-      {{ Form::hidden('id', $user->id) }}
-      {{ Form::button('フォローする', ['class' => 'btn', 'type' => 'submit']) }}
-      {{ Form::close() }}
-
-    @endif
-</tr>
+  <div class="search-commit">
+    <p>
+      <img class="search-icon icon" src="{{ asset('images/' . $user->images) }}", alt="{{ $user->images }}">
+    </p>
+    <p class="search-name">{{ $user->username }}</p>
+      @if($follows->contains($user->id))
+        {{ Form::open(['url' => '/unfollow', 'method' => 'post']) }}
+        {{ Form::hidden('id', $user->id) }}
+        {{ Form::button('フォローをはずす', ['class' => 'search-unfollow-button btn', 'type' => 'submit']) }}
+        {{ Form::close() }}
+      @else
+        {{ Form::open(['url' => '/follow', 'method' => 'post']) }}
+        {{ Form::hidden('id', $user->id) }}
+        {{ Form::button('フォローする', ['class' => 'search-follow-button btn', 'type' => 'submit']) }}
+        {{ Form::close() }}
+      @endif
+  </div>
 @endforeach
 
 @endsection
