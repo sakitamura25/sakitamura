a680e4ba2b4b50eccd2bff9ff01561aa118b4818
sakitamura25 <t.per125@icloud.com>
2023-02-06 23:48:50 +0900

    formファサードのnameを修正。

--- resources/views/posts/profile.blade.php
+++ resources/views/posts/profile.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-  {{ Form::open(['url' => '/profile', 'method' => 'post']) }}
+  {{ Form::open(['url' => '/profile', 'method' => 'post', 'enctype' => 'multipart/form-data']) }}
   {{ Form::hidden('id', $user->id) }}
 <div id="posts-profile-container">
   <p>
@@ -11,31 +11,31 @@
   <div class="posts-profile-block">
     <div class="posts-profile-form">
       {{ Form::label('username', 'UserName', ['class' => 'posts-profile-name']) }}
-      {{ Form::input('text', 'upUserName', $user->username, ['class' => 'posts-profile-text', 'required']) }}
+      {{ Form::input('username', null, $user->username, ['class' => 'posts-profile-text', 'required']) }}
     </div>
 
     <div class="posts-profile-form">
       {{ Form::label('mail', 'MailAddress', ['class' => 'posts-profile-name']) }}
-      {{ Form::input('mail', 'upMail', $user->mail, ['class' => 'posts-profile-text', 'required']) }}
+      {{ Form::input('mail', null, $user->mail, ['class' => 'posts-profile-text', 'required']) }}
     </div>
 
     <div class="posts-profile-form">
       {{ Form::label('password', 'Password', ['class' => 'posts-profile-name']) }}
-      {{ Form::input('password', '', $user->password, ['class' => 'posts-profile-text', 'readonly']) }}
+      {{ Form::input('password', null, $user->password, ['class' => 'posts-profile-text', 'readonly']) }}
     </div>
 
     <div class="posts-profile-form">
-      {{ Form::label('newpassword', 'new Password', ['class' => 'posts-profile-name']) }}
-      {{ Form::password('newPassword', ['class' => 'posts-profile-text']) }}
+      {{ Form::label('password', 'new Password', ['class' => 'posts-profile-name']) }}
+      {{ Form::password('password', ['class' => 'posts-profile-text']) }}
     </div>
 
     <div class="posts-profile-form">
       {{ Form::label('bio', 'Bio', ['class' => 'posts-profile-name']) }}
-      {{ Form::input('text', 'bio', $user->bio, ['class' => 'posts-profile-text']) }}
+      {{ Form::input('bio', null, $user->bio, ['class' => 'posts-profile-text']) }}
     </div>
 
     <div class="posts-profile-form">
-      {{ Form::label('image', 'Icon Image', ['class' => 'posts-profile-name']) }}
+      {{ Form::label('images', 'Icon Image', ['class' => 'posts-profile-name']) }}
       {{ Form::file('images', ['class' => 'posts-profile-text-img']) }}
     </div>
     <div class="posts-profile-button-block">
@@ -43,7 +43,16 @@
     </div>
   </div>
 </div>
-{{ Form::close() }}
+  {{ Form::close() }}
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
 @endsection
